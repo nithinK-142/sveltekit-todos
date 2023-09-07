e92ec26aa3155837874fcd32d74646cf12a307e3
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-07 12:09:36 +0530

    update: improved login page with better colors and icon buttons.

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -3,6 +3,8 @@
   import { isLoggedIn, user } from "../stores";
   import { signInWithPopup } from "firebase/auth";
   import Todos from "$lib/Todos.svelte";
+  import Google from "../assets/gg.png"
+  import GitHub from "../assets/github.svg"
 
     // let localUID = "";
     // let localUserName = "";
@@ -66,36 +68,50 @@
 {:else}
 
   <section>
-    <div
-      class="flex flex-col items-center justify-center gap-4 mt-20 text-center"
-    >
+
+    <div class="flex flex-col items-center justify-center gap-4 mt-32 text-center">
       <h1
-        class="bg-gradient-to-r from-teal-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text text-4xl font-bold font-mono"
+        class="bg-gradient-to-r from-teal-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text text-3xl sm:text-4xl font-bold font-mono"
       >
         Sveltkit & Firebase
       </h1>
       <h2
-        class="bg-gradient-to-r from-teal-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text text-2xl font-bold font-mono px-3"
+        class="bg-gradient-to-r from-teal-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text text-2xl sm:text-3xl font-bold font-mono px-3"
       >
         Todo App
       </h2>
     </div>
 
-    <div class="sm:flex sm:justify-center m-10 gap-5">
-      <button
-        class="block rounded-md bg-yellow-500 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-yellow-700 lg:text-md"
-        on:click={() => login("Google")}
-      >
-        Google
-      </button>
+    <div class="w-full flex justify-center mt-10">
+
+      <div class="flex flex-col bg-[hsla(0,0%,100%,.8)] py-4 px-8 rounded-lg">
+        <h3 class="text-black text-center font-bold">Login</h3>
+        
+        <div class="flex justify-center m-4 space-x-6">
+          <span class="w-12 h-12 bg-white hover:bg-teal-100 hover:scale-110 rounded-xl select-none">
+            <button
+              class="bg-opacity-0 cursor-pointer"
+              on:click={() => login("Google")}
+            >
+              <img src={Google} alt="Google" class="w-12 h-12" />
+            </button>
+          </span>
     
-      <button
-        class="block rounded-md bg-yellow-500 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-yellow-700 lg:text-md"
-        on:click={() => login("GitHub")}
-      >
-        Github
-      </button>
+          <span class="w-12 h-12 bg-white hover:bg-teal-100 hover:scale-110 rounded-xl select-none flex align-center justify-center">
+            <button
+              class="bg-opacity-0 cursor-pointer"
+              on:click={() => login("GitHub")}
+            >
+              <img
+                src={GitHub}
+                alt="GitHub"
+                class="w-8 h-8"
+              />
+            </button>
+          </span>
+      </div>
     </div>
+
   </section>
 
 {/if}
